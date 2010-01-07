75b0f76d97b1174b167922fb6beb529db77f11b7
Jaakko Suutarla <jaakko.suutarla@bf-engineering.fi>
2010-01-07 15:58:46 +0200

    Added temp graph

--- app/controllers/tempatures_controller.rb
+++ app/controllers/tempatures_controller.rb
@@ -1,11 +1,18 @@
 class TempaturesController < ApplicationController
+  
+  use_google_charts
+  
   def add
     @tempature = Tempature.create({:cottage_id => params[:cottage_id], 
         :temp => params[:temp].tr(',','.')})
   end
 
   def index
-    
+    puts Tempature.find(:all).each{|t| t.temp.to_f}.collect
+    dataset = GoogleChartDataset.new :data => Tempature.find(:all).collect{|t| t.temp.to_f }.collect
+    data = GoogleChartData.new :datasets => dataset
+    @chart = GoogleLineChart.new :width => 300, :height => 200
+    @chart.data = data
   end
 
 end
