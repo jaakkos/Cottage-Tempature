e1389a52694af17fcbb1f98a8babfecf481f53bb
Jaakko Suutarla <jaakko.suutarla@bf-engineering.fi>
2010-01-07 16:38:01 +0200

    Added

--- app/controllers/tempatures_controller.rb
+++ app/controllers/tempatures_controller.rb
@@ -8,11 +8,17 @@ class TempaturesController < ApplicationController
   end
 
   def index
-    puts Tempature.find(:all).each{|t| t.temp.to_f}.collect
-    dataset = GoogleChartDataset.new :data => Tempature.find(:all).collect{|t| t.temp.to_f }.collect
-    data = GoogleChartData.new :datasets => dataset
-    @chart = GoogleLineChart.new :width => 300, :height => 200
+    dataset1 = GoogleChartDataset.new :data => 
+      Tempature.find(:all).collect{|t| t.temp.to_f }.collect
+    dataset2 = GoogleChartDataset.new :data => 
+      Tempature.find(:all).collect{|t| t.created_at.strftime '%m/%d/%Y' }.collect
+    data = GoogleChartData.new :datasets => dataset1
+    axis = GoogleChartAxis.new :axis  => [GoogleChartAxis::LEFT]
+    axis_labels = GoogleChartAxis.new :labels => dataset2
+    @chart = GoogleLineChart.new :width => 500, :height => 200, :title => "Cottage Tempature"
     @chart.data = data
+    @chart.axis = axis
+
   end
 
 end
